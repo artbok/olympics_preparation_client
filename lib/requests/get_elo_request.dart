3c8040647b3ccaecc9580de815385bbe3d343823
Temjik <artem.bokans@gmail.com>
2026-01-28 12:53:55 +0300

    duels

---
+++ lib/requests/get_elo_request.dart
@@ -0,0 +1,17 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:olympics_preparation_client/localstorage.dart';
+
+Future<int> getElo(String username) async {
+  Map<String, dynamic> params = {"username": username};
+
+  final Uri url = Uri.parse('${getValue("serverAddress")}/getElo');
+
+  final response = await http.post(
+    url,
+    headers: <String, String>{'Content-Type': 'application/json'},
+    body: json.encode(params),
+  );
+  Map<String, dynamic> data = jsonDecode(response.body) as Map<String, dynamic>;
+  return data["elo"];
+}
