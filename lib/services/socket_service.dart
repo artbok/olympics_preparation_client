2584d095ebb0fc8d996cefb3c54fe27665f839e6
Temjik <artem.bokans@gmail.com>
2026-02-05 14:55:27 +0300

    duels

--- lib/services/socket_service.dart
+++ lib/services/socket_service.dart
@@ -1,28 +1,52 @@
 import 'package:socket_io_client/socket_io_client.dart';
 import 'package:olympics_preparation_client/localstorage.dart';
+import 'dart:convert';
+import 'dart:async';
+import 'package:flutter/material.dart';
 
 class SocketService {
-  late Socket socket;
+  static final SocketService _instance = SocketService._internal();
+  factory SocketService() => _instance;
+  SocketService._internal();
 
+  late Socket socket;
+  final ValueNotifier<Map<String, dynamic>?> notifier =
+      ValueNotifier<Map<String, dynamic>?>(null);
   void connectToServer(String username) {
     socket = io(
       "${getValue("serverAddress")}",
-      OptionBuilder()
-          .setTransports(['websocket'])
-          .disableAutoConnect()
-          .build(),
+      OptionBuilder().setTransports(['websocket']).disableAutoConnect().build(),
     );
     socket.connect();
 
     socket.onConnect((_) {
       print('Connected to backend');
-      socket.emit('join', username);
+      socket.emit('matchmaking', username);
+    });
+    socket.on("matchmaking_$username", (data) {
+      print("что то пришло");
+      switch (data["code"]) {
+        case "match_found":
+          {
+            notifier.value = {
+              "page": "game_page",
+              "opponent": data["opponent"]["name"],
+              "rating": data["opponent"]["rating"],
+            };
+          }
+      }
     });
   }
-  
+
+  void connectToDuel(String duelName) {
+    socket.on(duelName, (data) {
+      // startRound
+      // answer
+      // 
+    });
+  }
+
   void sendMessage(String event, dynamic data) {
     socket.emit(event, data);
   }
 }
-
-
